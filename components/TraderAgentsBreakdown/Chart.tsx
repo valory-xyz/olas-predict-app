be7b121916d368f468e3a05d3708cd3e46e78e57
mohandast52 <mohandast52@gmail.com>
2024-10-10 09:50:25 +0200

    feat: shaky animation

--- components/TraderAgentsBreakdown/Chart.tsx
+++ components/TraderAgentsBreakdown/Chart.tsx
@@ -53,30 +53,97 @@ export const Chart = () => {
       strokeWidth: 1,
       strokeOpacity: 0,
     });
-    series.circles.template.adapters.add('fill', (fill, target) => {
-      const dataItem = target.dataItem;
-      if (dataItem) {
-        const totalBets = (dataItem.dataContext as TraderAgentInfo).totalBets;
-        if (totalBets < 100) {
-          return am5.color(0x0000ff); // blue
-        } else if (totalBets > 100 && totalBets < 200) {
-          return am5.color(0x00ff00); // green
-        } else if (totalBets >= 200 && totalBets < 300) {
-          return am5.color(0xffff00); // yellow
-        } else if (totalBets >= 300 && totalBets < 400) {
-          return am5.color(0xffa500); // orange
-        } else if (totalBets >= 400 && totalBets < 500) {
-          return am5.color(0xff0000); // red
-        } else if (totalBets >= 500) {
-          return am5.color(0x8a2be2); // blueviolet
-        }
+
+    // series.circles.template.adapters.add('fill', (fill, target) => {
+    //   const dataItem = target.dataItem;
+    //   if (dataItem) {
+    //     const totalBets = (dataItem.dataContext as TraderAgentInfo).totalBets;
+    //     if (totalBets < 100) {
+    //       return am5.color(0x0000ff); // blue
+    //     } else if (totalBets > 100 && totalBets < 200) {
+    //       return am5.color(0x00ff00); // green
+    //     } else if (totalBets >= 200 && totalBets < 300) {
+    //       return am5.color(0xffff00); // yellow
+    //     } else if (totalBets >= 300 && totalBets < 400) {
+    //       return am5.color(0xffa500); // orange
+    //     } else if (totalBets >= 400 && totalBets < 500) {
+    //       return am5.color(0xff0000); // red
+    //     } else if (totalBets >= 500) {
+    //       return am5.color(0x8a2be2); // blueviolet
+    //     }
+    //   }
+    //   return fill;
+    // });
+
+    setInterval(async () => {
+      const allInnerCircles = series.circles;
+      // console.log('allInnerCircles', allInnerCircles);
+      const circle = allInnerCircles.values[1];
+      // allInnerCircles._values.forEach((circle) => {
+      // const circle = dataItem.get('circle');
+      // console.log('dataItem', dataItem);
+
+      // const circle = dataItem.get('circle');
+      // console.log('circle', circle);
+
+      // const dataContext = dataItem.dataContext as TraderAgentInfo;
+      // const currentTotalBets = dataContext.totalBets;
+      // console.log('circle', currentTotalBets);
+
+      if (circle) {
+        const animation = circle.animate({
+          key: 'y',
+          from: -20,
+          to: 20,
+          loops: 4,
+          duration: 100,
+          easing: am5.ease.yoyo(am5.ease.cubic),
+        });
+
+        await new Promise((resolve) => setTimeout(resolve, 300)); // 3 sec
+
+        // animation.events.on('stopped', () => {
+        circle.animate({
+          key: 'y',
+          to: 0,
+          duration: 50,
+          easing: am5.ease.out(am5.ease.cubic),
+        });
+        // .play();
+        // });
+
+        animation.play();
       }
-      return fill;
-    });
+
+      // dataItem.set('value', currentTotalBets);
+      // Update the value (if necessary) in the dataItem
+      // const dataContext = dataItem.dataContext as TraderAgentInfo;
+      // const currentTotalBets = dataContext.totalBets;
+      // dataItem.set('value', currentTotalBets);
+      // });
+    }, 2000);
+    // });
+
+    // Simulate data validation after a delay
+    // setInterval(() => {
+    //   // Update the data
+    //   series.data.setIndex(0, {
+    //     name: 'root',
+    //     totalBets: Math.floor(Math.random() * 1000),
+    //   });
+
+    //   // Dispatch the dataitemsvalidated event
+    //   if (!root.isDisposed()) {
+    //     series.events.dispatch('datavalidated', {
+    //       type: 'datavalidated',
+    //       target: series,
+    //     });
+    //   }
+    // }, 2000);
 
     series.data.setAll([
       {
-        name: 'root',
+        name: 'Root',
         children: chartData,
       },
     ]);
